19ac50fe25ba122a2e44f531b17c56d3800dca09
debz-g <debz.exe@gmail.com>
2023-05-24 22:44:08 +0530

    Added:
    - Boht saare changes phir se

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_dev_redfox_networkaccessstorage_NetwrokAccessStorage.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "dev.redfox.networkaccessstorage.NetwrokAccessStorage"
-)
-public final class _dev_redfox_networkaccessstorage_NetwrokAccessStorage {
-}
--- app/build/generated/source/kapt/debug/dev/redfox/networkaccessstorage/BR.java
+++
@@ -1,5 +0,0 @@
-package dev.redfox.networkaccessstorage;
-
-public class BR {
-  public static final int _all = 0;
-}
